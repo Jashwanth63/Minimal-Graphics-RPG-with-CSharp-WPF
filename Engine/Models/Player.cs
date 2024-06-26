5bb0adf56b2bc73cb4be5449d904874e5022f6f8
= <56686212+Jashwanth63@users.noreply.github.com>
2024-06-25 20:21:24 -0400

    Binded ViewModel to View.

--- Engine/Models/Player.cs
+++ Engine/Models/Player.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Engine.Models
 {
-    internal class Player
+    public class Player
     {
         public String Name { get; set; }
         public String CharacterClass { get; set; }
